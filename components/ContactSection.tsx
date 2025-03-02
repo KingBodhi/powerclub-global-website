7dff7e0ef2a938ecea9bfe91ef35a4f386b7b7d1
Dksie09 <dakshiegoel@gmail.com>
2025-03-02 22:28:06 +0530

    contact form fix

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -208,16 +208,14 @@ const ContactSection: React.FC = () => {
         <div className="container mx-auto px-4">
           <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-12 gap-8">
             {/* Left Column */}
-            <div className="lg:col-span-5 grid grid-rows-2 gap-6">
+            <div className="lg:col-span-5 flex flex-col gap-6">
               <HighlightCard />
-              <div className="grid grid-rows-2 gap-6">
-                <ContactCard
-                  icon={Mail}
-                  title="Email Us"
-                  info="contact@example.com"
-                  primary
-                />
-              </div>
+              <ContactCard
+                icon={Mail}
+                title="Email Us"
+                info="contact@example.com"
+                primary
+              />
             </div>
 
             {/* Right Column - Contact Form */}
