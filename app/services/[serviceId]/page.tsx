248a2e054b904ebbe77b5d11f8a3eedd9cd3e4f7
Dksie09 <dakshiegoel@gmail.com>
2025-03-02 22:36:13 +0530

    remove teammates

--- app/services/[serviceId]/page.tsx
+++ app/services/[serviceId]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import Navbar from "@/components/Navbar";
-import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+// import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 import {
   ArrowUpRight,
@@ -26,43 +26,43 @@ import FAQSection from "@/components/FAQSection";
 import WorkflowSection from "@/components/WorkflowSection";
 import { FaSchoolFlag } from "react-icons/fa6";
 
-const people = [
-  {
-    id: 1,
-    name: "John Doe",
-    designation: "Founder & CEO",
-    image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-  },
-  {
-    id: 2,
-    name: "Robert Johnson",
-    designation: "CTO",
-    image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 3,
-    name: "Jane Smith",
-    designation: "CO-Founder",
-    image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 4,
-    name: "Emily Davis",
-    designation: "CFO",
-    image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 5,
-    name: "Tyler Durden",
-    designation: "CEO",
-    image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
-  },
-];
+// const people = [
+//   {
+//     id: 1,
+//     name: "John Doe",
+//     designation: "Founder & CEO",
+//     image:
+//       "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+//   },
+//   {
+//     id: 2,
+//     name: "Robert Johnson",
+//     designation: "CTO",
+//     image:
+//       "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+//   },
+//   {
+//     id: 3,
+//     name: "Jane Smith",
+//     designation: "CO-Founder",
+//     image:
+//       "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+//   },
+//   {
+//     id: 4,
+//     name: "Emily Davis",
+//     designation: "CFO",
+//     image:
+//       "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//   },
+//   {
+//     id: 5,
+//     name: "Tyler Durden",
+//     designation: "CEO",
+//     image:
+//       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+//   },
+// ];
 
 interface ServiceCardProps {
   title: string;
@@ -207,9 +207,9 @@ export default async function ServicePage({ params }: PageProps) {
               {service.suffix}
             </h1>
 
-            <div className="flex gap-3 mt-8 lg:mt-12">
+            {/* <div className="flex gap-3 mt-8 lg:mt-12">
               <AnimatedTooltip items={people} />
-            </div>
+            </div> */}
             <p className="text-white/70 text-base lg:text-lg max-w-xl mt-8 lg:mt-12">
               {service.description}
             </p>
